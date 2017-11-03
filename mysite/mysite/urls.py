a1f3fda45165e4908914ccf01436080b95226eb8
Rebecca03 <tmancraig03@gmail.com>
2017-11-03 12:29:47 -0400

    Added login/signup pages while also creating an accounts app to track users and to manage their profiles

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('content.urls')),
-    url(r'^signup/', views.signup, name='signup')
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, name='logout'),
     #url(r'^blog/', include('blog.urls')),
 ]
