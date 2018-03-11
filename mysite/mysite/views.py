4bc605c886b8261e617b480a1263804bedde17de
Rebecca03 <tmancraig03@gmail.com>
2018-03-11 15:59:08 -0400

    fixed an issue where the wrong method was being called. Also a database issue when fetching - need to address it by setting up a proper git ignore

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,7 +13,7 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            lg(request, user)
             return redirect('/')
     else:
         form = UserCreationForm()
